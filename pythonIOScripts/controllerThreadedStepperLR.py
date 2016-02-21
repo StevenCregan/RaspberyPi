04c8a46c070d05481ca41f20c97026fb3ae4b58c
StevenCregan <stevencregan@gmail.com>
2016-02-21 11:10:05 +0000

    threaded motor support

--- pythonIOScripts/controllerThreadedStepperLR.py
+++ pythonIOScripts/controllerThreadedStepperLR.py
@@ -3,6 +3,9 @@ import RPi.GPIO as GPIO
 import time
 import os, sys
 from multiprocessing import Process
+import threading
+# Credit to Zephod on github
+# https://github.com/zephod/lego-pi
 sys.path.insert(0, '/home/pi/')
 from legopi.lib import xbox_read
 GPIO.setmode(GPIO.BCM)
@@ -52,8 +55,8 @@ def backward():
     setStep(1,1,0,0)
     time.sleep(delay)
 
-def forward():
-  while True:
+def forward(arg1, stop_event):
+  while(not stop_event.is_set()):
     time.sleep(delay)
     setStep(1,0,0,0)
     time.sleep(delay)
@@ -83,22 +86,33 @@ def off():
   time.sleep(delay)
 
 if __name__ == "__main__":
+  #store controller values
   temp = ""
+  A = 1
   pForward = Process(target=forward)
+  tForward_stop = threading.Event()
+  tForward = threading.Thread(target = forward, args=(1, tForward_stop))
+  tForward.daemon = True
   while True:
    for event in xbox_read.event_stream(deadzone=12000):
     temp = str(event)
     print temp
-    
+    print A
     input_state_L = GPIO.input(lbutton)
     input_state_R = GPIO.input(rbutton)
     print ("{0}  ,  {1}").format(input_state_L, input_state_R)
     
     if(temp == "Event(A,1,0)"):
+        A = 0
         input_state_L = 0
-    else:
+    elif(temp == "Event(A,0,1)"):
+        A = 1
         input_state_L = 1
-    
+    else:
+        input_state_L = A
+        pass
+        
+    print input_state_L
     print ("{0}  ,  {1}").format(input_state_L, input_state_R)
 
     if (input_state_L == False and input_state_R == False):
@@ -109,14 +123,20 @@ if __name__ == "__main__":
     elif (input_state_L == False and input_state_R == True):
         GPIO.output(rled,0)
         GPIO.output(lled,1)
-        #forward()
         print "forward"
-        if (pForward.is_alive()!=True):
-          pForward.start()
-        time.sleep(0.1)
-#        pForward.terminate()
-        time.sleep(0.1)
-        print pForward, pForward.is_alive()
+        print (threading.enumerate())
+        try:
+          if (tForward.is_alive()!=True):
+            print ('start thread')
+            tForward_stop.clear()
+            print tForward_stop.is_set()
+            tForward = threading.Thread(target = forward, args=(1, tForward_stop))
+            tForward.start()
+            print (threading.enumerate())
+        except:
+          print('error occured')
+          print sys.exc_info()
+          pass
     elif (input_state_R == False and input_state_L == True):
         GPIO.output(lled,0)
         GPIO.output(rled,1)
@@ -125,4 +145,13 @@ if __name__ == "__main__":
     else:
         GPIO.output(lled,0)
         GPIO.output(rled,0)
+        print('button stopped')
+        print(threading.enumerate())
+        try:
+#          if (tForward.is_alive()==True):
+            tForward_stop.set()
+            tForward.join()
+        except:
+          print('error occured')
+          print sys.exc_info()
         off()
